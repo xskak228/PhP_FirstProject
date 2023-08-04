4545cb4b92e93d7e762146ba2379f36212f55a12
TPLV <81437015+xskak228@users.noreply.github.com>
2023-08-05 00:11:09 +0300

    1

--- includes/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$link = mysqli_connect("localhost", "root", "", "notes");
-if (!$link){
-    echo "Ошибка: Невозможно подключиться к MySQL <br>";
-    echo mysqli_connect_error();
-    exit();
-}
--- index.php
+++ index.php
@@ -1,18 +1,81 @@
 <?php
-include('includes/db.php');
 
-$results = mysqli_query($link, "SELECT * FROM `Notes`");
-while (($rec = mysqli_fetch_assoc($results)))
+class User
 {
-    echo '<b>id: </b>' . $rec['id'] . "<br>";
-    echo '<b>title: </b>' . $rec['title'] . "<br>";
-    echo '<b>text: </b>' . $rec['text'] . "<br>";
-    echo "<hr>";
+    public $email;
+    public $password;
+    public $username;
+
+    public function __construct($email, $password, $username)
+    {
+       $this->email = $email;
+       $this->password = $password;
+       $this->username = $username;
+    }
+
+    # не буду учитывать "предыдуший email и ник"
+    public function ChangeEmail($newEmail) {$this->email = $newEmail;}
+    public function ChangeUserName($newUserName) {$this->username = $newUserName;}
+
 }
-?>
-<br>
-<form method="post" action="/handle.php">
-    <p>title: <input type="text" name="title"></p>
-    <p>text: <input type="text" name="text"></p>
-    <input type="submit">
-</form>
+
+class News
+{
+    public $content;
+    public $title;
+    public $author;
+    public $category;
+    public $comments = [];
+
+    public function __construct($title, $content, $category, $author)
+    {
+        $this->title = $title;
+        $this->content = $content;
+        $this->category = $category;
+        $this->author = $author;
+    }
+
+    public function AddComment($comment) {$this->comments[] = $comment;}
+    public function EditingNews($newTitle, $newContent, $newCategory, $author)
+    {
+        $this->title = $newTitle;
+        $this->content = $newContent;
+        $this->category = $newCategory;
+        $this->author = $author;
+    }
+}
+
+class Comments
+{
+    public $content;
+    public $author;
+
+    public function __construct($content, $author)
+    {
+        $this->content = $content;
+        $this->author = $author;
+    }
+}
+
+class Guest {
+    # елси бы я разробатывал гостя, то добавил бы id у него
+    # не знаю, хорошая практика или нет...
+    public $userId;
+
+    public function __construct($userId) {$this->userId = $userId;}
+
+}
+
+$user = new User("test@test.ru", "1234567890", "testname");
+$user->ChangeUserName("NewNick");
+$user->ChangeEmail("new@new.ru");
+
+$guest = new Guest("1234567890"); # и это id генерится по своим правилам
+
+$news = new News("Test", "123", "test", $user);
+$news->EditingNews("Test2", "321", "test2", $user);
+# есть сомнения в правильности изменения, в плане, "$user"
+
+$comment = new Comments("123123", $user);
+
+$news->AddComment($comment);
